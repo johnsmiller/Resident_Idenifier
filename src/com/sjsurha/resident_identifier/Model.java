a90ff6f039c5df9bcbec25a4e47c36bbf5c31620
John <John@192.168.5.18>
2014-03-29 21:22:04 -0700

    Implementations and Code updates Implemented Merge Database function (Model & ViewEditDatabase) added/corrected JavaDocs modified Model's Initialize, Seal, and Unseal functions to be static so they can be used by merge function
    Future updates:
    Get GNU Licensing completely taken care of
    Remove ability to access model from JComboBoxes and JTables
    Perhaps move resident import function to external class to improve model readability
    Move getEventWaitlistJTable and getEventAttendeesJTable implementations from model to event class
    Update event class to store resident contact information (email, phone number?)
    	Update import function
    	Update display functions
    	Create function to get formatted list of contact information of all attendees
    Add warning on tickets pane about additional attendees/waitlists after random ticket pool is generated
    Implement building specific check-in
    	maintain Hash(Tree?) set of all buildings (first three letters of all bedspaces)
    	modify check-in gui to support disallowing / allowing certain buildings
    Continue to bash head against wall in relation to server / client implementation
    	Half-way host that is hosted on public webserver
    		does not save any client/server communications except for short time
    		does not have ability to decrypt / interpret messages, only knows who (server, clients) they're for 
    		communications discarded once intended recipiant is contacted
    		webserver service is only active when needed, stopped when all clients / servers are inactive
    		serves as half-way point for clients/servers behind firewalls.
    		possible platform for mobile app communication in future

--- src/com/sjsurha/resident_identifier/Model.java
+++ src/com/sjsurha/resident_identifier/Model.java
@@ -28,6 +28,7 @@ import java.util.TreeMap;
 import java.util.TreeSet;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.ComboBoxModel;
 import javax.swing.JComboBox;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
